c9bb69647a05e903bae51042d0bc92e422b3a0e1
Greg Atkins <greg@ingenii.dev>
2022-01-21 14:15:02 +0000

    Test entry creation

--- unit_tests/test_import_entry.py
+++ unit_tests/test_import_entry.py
@@ -1,14 +1,19 @@
+from datetime import datetime
+from pyspark.sql.types import StructField, StructType, IntegerType, \
+    StringType, TimestampType
 import sys
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
 sys.modules["delta.tables"] = Mock()
-sys.modules["pyspark"] = Mock()
-sys.modules["pyspark.sql"] = Mock()
-sys.modules["pyspark.sql.dataframe"] = Mock()
-sys.modules["pyspark.sql.functions"] = Mock()
-sys.modules["pyspark.sql.session"] = Mock()
-sys.modules["pyspark.sql.types"] = Mock()
+# sys.modules["pyspark"] = Mock()
+# sys.modules["pyspark.sql"] = Mock()
+# sys.modules["pyspark.sql.dataframe"] = Mock()
+sys.modules["pyspark.sql.functions"] = functions_mock = Mock(
+    hash=Mock(return_value="mock hash")
+)
+# sys.modules["pyspark.sql.session"] = Mock()
+# sys.modules["pyspark.sql.types"] = pyspark_sql_types_mock = Mock()
 
 from ingenii_databricks.orchestration import ImportFileEntry  # noqa: E402
 from ingenii_databricks.orchestration.import_file import \
@@ -46,6 +51,7 @@ class TestInitialisation(TestCase):
             )
         )
     )
+    functions_mock = Mock()
 
     def test_entry_doesnt_exist_create(self):
         """ Creates new entry if one doesn't exist """
@@ -236,3 +242,89 @@ class TestInitialisation(TestCase):
             )
 
         get_import_entry_mock.assert_not_called()
+
+    def test_create_import_entry_data(self):
+        """ Creates new entry if one doesn't exist """
+
+        path_exists_mock = Mock(return_value=True)
+        get_import_entry_mock = Mock()
+        get_import_table_mock = Mock()
+        new_entry_mock = Mock(
+            columns=["col1", "col2"],
+            first=Mock(return_value=Mock(hash=self.row_hash))
+        )
+        spark_mock = Mock(
+            createDataFrame=Mock(
+                return_value=Mock(
+                    withColumn=Mock(
+                        return_value=new_entry_mock
+                    )
+                )
+            )
+        )
+
+        with \
+                patch(f"{class_str}.get_import_table_df", self.no_import_entry_mock), \
+                patch(f"{class_str}.get_import_entry", get_import_entry_mock), \
+                patch(f"{class_str}.get_import_table", get_import_table_mock), \
+                patch("os.path.exists", path_exists_mock):
+            import_entry = ImportFileEntry(
+                spark=spark_mock,
+                source_name=self.source_name, table_name=self.table_name,
+                file_name=self.file_name, increment=self.increment,
+            )
+
+        # Check import entry creation
+        spark_mock.createDataFrame.assert_called_once()
+        df_args, df_kwargs = spark_mock.createDataFrame.call_args_list[0]
+        self.assertTupleEqual(df_args, ())
+        self.assertEqual(len(df_kwargs["data"]), 1)
+        for data, col in zip(df_kwargs["data"][0], df_kwargs["schema"]):
+            if col.name == "source":
+                self.assertEqual(data, self.source_name)
+            elif col.name == "table":
+                self.assertEqual(data, self.table_name)
+            elif col.name == "file_name":
+                self.assertEqual(data, self.file_name)
+            elif col.name == "processed_file_name":
+                self.assertEqual(data, None)
+            elif col.name == "increment":
+                self.assertEqual(data, self.increment)
+            elif col.name == "date_new" or col.name == "_date_row_inserted":
+                self.assertTrue(isinstance(data, datetime))
+            else:
+                raise Exception(f"Unexpected column: {col.name}, {data}")
+
+        # Check hash column addition
+        spark_mock.createDataFrame.return_value.withColumn.assert_called_once()
+        wc_args, wc_kwargs = \
+            spark_mock.createDataFrame.return_value.withColumn.call_args_list[0]
+        self.assertTupleEqual(wc_args, ("hash", functions_mock.hash.return_value))
+        self.assertDictEqual(wc_kwargs, {})
+
+        hash_args, hash_kwargs = functions_mock.hash.call_args_list[-1]
+        self.assertTupleEqual(hash_args, ("source", "table", "file_name"))
+        self.assertDictEqual(hash_kwargs, {})
+
+        # Check merge
+        get_import_table_mock.assert_called_once_with()
+
+        get_import_table_mock.return_value.alias.assert_called_once_with("target")
+        new_entry_mock.alias.assert_called_once_with("new_data")
+
+        merge_call = get_import_table_mock.return_value.alias.return_value.merge
+        merge_call.assert_called_once_with(
+            new_entry_mock.alias.return_value,
+            "target.hash = new_data.hash AND target.increment = new_data.increment"
+        )
+
+        when_not_matched_insert_call = merge_call.return_value.whenNotMatchedInsert
+        when_not_matched_insert_call.assert_called_once_with(values={
+            col: f"new_data.{col}" for col in new_entry_mock.columns
+        })
+
+        when_not_matched_insert_call.return_value.execute.assert_called_once_with()
+
+        # Finally
+        get_import_entry_mock.assert_called_once()
+        self.assertEqual(import_entry.hash, self.row_hash)
