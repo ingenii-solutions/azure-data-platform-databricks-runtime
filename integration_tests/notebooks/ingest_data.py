2e54c80354fcb28a379b2858b17398e2e4716085
Greg Atkins <greg@ingenii.dev>
2022-02-01 17:36:55 +0000

    Add extra columns test

--- integration_tests/notebooks/ingest_data.py
+++ integration_tests/notebooks/ingest_data.py
@@ -6,11 +6,17 @@ from ingenii_data_engineering.dbt_schema import get_source
 from ingenii_databricks.enums import Stage
 from ingenii_databricks.orchestration import ImportFileEntry
 from ingenii_databricks.pipeline import archive_file, create_file_table
+from ingenii_databricks.table_utils import read_file, SchemaException
+
+# COMMAND ----------
 
 dbt_root_folder = environ["DBT_ROOT_FOLDER"]
 source = "test_data"
 
 source_details = get_source(dbt_root_folder, source)
+
+# COMMAND ----------
+# Create normal file table
 
 table0_name = "table0"
 table0_schema = source_details["tables"][table0_name]
@@ -25,3 +31,18 @@ if import_entry.is_stage(Stage.NEW):
     import_entry.update_status(Stage.ARCHIVED)
 
 n_rows = create_file_table(spark, import_entry, table0_schema)
+
+# COMMAND ----------
+# Catch when there are extra columns in the file
+
+table_name = "table2"
+table_schema = source_details["tables"][table_name]
+file_name = "file2.csv"
+
+exception = False
+try:
+    read_file(spark, f"/mnt/raw/{source}/{table_name}/{file_name}", table_schema)
+except SchemaException:
+    exception = True
+
+assert exception
