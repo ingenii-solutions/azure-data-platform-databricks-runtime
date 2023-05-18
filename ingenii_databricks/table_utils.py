28ef89ef6a8514ad527231f4e02d9c7c352df8e7
Greg Atkins <greg@ingenii.dev>
2023-05-18 16:33:48 +0100

    Don't assume file table has all columns

--- ingenii_databricks/table_utils.py
+++ ingenii_databricks/table_utils.py
@@ -252,7 +252,7 @@ def read_file(spark: SparkSession, file_path: str, table_schema: dict
 
         # File may not have all columns or in different order
         if file_details.get("header"):
-            with open("/dbfs/" + file_path) as raw_file:
+            with open("/dbfs" + file_path) as raw_file:
                 headers = raw_file.readline().strip() \
                                   .split(file_details.get("sep", ","))
 
@@ -478,6 +478,10 @@ def merge_dataframe_into_table(merge_table: DeltaTable, dataframe: DataFrame,
         merge_table.alias("deltatable") \
                    .merge(dataframe.alias("dataframe"),
                           _match_condition_string(merge_columns))
+    update_columns = {
+        col_name: f"dataframe.{col_name}"
+        for col_name in dataframe.columns
+    }
 
     if merge_type == MergeType.MERGE_DATE_ROWS:
         # Merge, but be careful with _date_row_inserted and _date_row_updated
@@ -485,32 +489,34 @@ def merge_dataframe_into_table(merge_table: DeltaTable, dataframe: DataFrame,
         updated_table = updated_table \
             .whenNotMatchedInsert(
                 values={
-                    col_name: f"dataframe.{col_name}"
-                    for col_name in dataframe.columns
-                    if col_name != ic.DATE_ROW_UPDATED  # _date_row_updated is null
+                    col_name: v
+                    for col_name, v in update_columns.items()
+                    if col_name != ic.DATE_ROW_UPDATED
+                    # _date_row_updated is null
                 }
             ) \
             .whenMatchedUpdate(
                 condition=_difference_condition_string(
                     dataframe.columns, merge_columns),
                 set={
-                    col_name: f"dataframe.{col_name}"
-                    for col_name in dataframe.columns
+                    col_name: v
+                    for col_name, v in update_columns.items()
                     if col_name != ic.DATE_ROW_INSERTED  # Remains the same
                 }
             )
     elif merge_type == MergeType.MERGE_UPDATE:
         # Insert, or update if any of the data columns change
         updated_table = updated_table \
-            .whenNotMatchedInsertAll() \
-            .whenMatchedUpdateAll(
+            .whenNotMatchedInsert(values=update_columns) \
+            .whenMatchedUpdate(
                 condition=_difference_condition_string(
-                    dataframe.columns, merge_columns)
+                    dataframe.columns, merge_columns),
+                values=update_columns
             )
     elif merge_type == MergeType.MERGE_INSERT:
         # Only insert
         updated_table = updated_table \
-            .whenNotMatchedInsertAll()
+            .whenNotMatchedInsert(values=update_columns)
     else:
         raise Exception(
             f"{merge_type} not a recognised merge type! "
